52e9b53e228a5453ef311418eba4548046f36f05
Roberto Ribeiro <robertov82008@gmail.com>
2017-12-20 19:51:17 -0200

    aula 038

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ActiveRecord::Base
   belongs_to :kind
+  has_one :address
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,3 @@ Kind.create!([
   {description: 'Comercial'}
 ])
 puts 'Gerando os tipos de contato (Kinds)... [OK]'
-
-puts 'Gerando os contatos (Contacts)...'
-10.times do |i| 
-  Contact.create!(
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    kind: Kind.all.sample,
-    rmk: Faker::Lorem.paragraph([1, 2, 3, 4, 5].sample)
-  )
-end
-puts 'Gerando os contatos (Contacts)... [OK]'
-
